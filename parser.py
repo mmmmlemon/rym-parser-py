5ac156889a3337833b4b596f5466d13f65cb541d
EgoriousZhuzh <egor_005@mail.ru>
2019-07-19 20:26:52 +1000

    Изменение имени файла

--- parser.py
+++ parser.py
@@ -1,70 +1,76 @@
 #импорт библиотеки для чтения с кодировками
 import io
+import os
 #импорт бибилиотеки для рисования таблиц
 from tabulate import tabulate
 import math
 
 #ф-ция, загрузить файл и преобразовать его в массив для последующей работы
 def load_file(filename):
-    #открываем файл и читаем его содержимое
-    file = io.open(filename, "r", encoding='utf8')
 
-    #сохраняем содержимое файла в переменной строки
-    file_string = file.read()
+    if(os.path.isfile("./"+filename+".txt")):
+        #открываем файл и читаем его содержимое
+        file = io.open(filename+".txt", "r", encoding='utf8')
 
-    #считаем количество строк в тексте
-    num_of_lines = 0
-    with io.open(filename, "r", encoding='utf8') as f:
-        num_of_lines = sum(1 for _ in f)
+        #сохраняем содержимое файла в переменной строки
+        file_string = file.read()
 
-    #ищем индекс начала слова Review, принимаем его за начальный, с него начинаем читать файл
-    index_start = file_string.find("Review")
+        #считаем количество строк в тексте
+        num_of_lines = 0
+        with io.open(filename + ".txt", "r", encoding='utf8') as f:
+            num_of_lines = sum(1 for _ in f)
 
-    #общий массив для всех альбомов
-    album_array = []
+        #ищем индекс начала слова Review, принимаем его за начальный, с него начинаем читать файл
+        index_start = file_string.find("Review")
 
-    #начинаем цикл, ходим по строкам и записываем данные в общий массив
-    for i in range (1, num_of_lines):
-        #массив для текущего альбома в цикле
-        current_album = []
+        #общий массив для всех альбомов
+        album_array = []
 
-        #в каждой строке 12 параметров, вытаскиваем их все
-        for a in range (0, 12):
-            #определяем начало и конец кажого параметра при помощи кавычек
-            #кавычка в начале
-            idx_firstQ = file_string.find('\"', index_start) + 1
-            #кавычка в конце
-            idx_lastQ = file_string.find('\"', idx_firstQ)
-            #получаем подстроку при помощи индексов кавычек
-            txt = file_string[idx_firstQ:idx_lastQ]
-            if txt != "" and a != 10 and a != 8 or a == 6 or a == 1 or a == 2 or a == 3 or a == 4:
-                current_album.append(txt)
-            
-            index_start = file_string.find(",",idx_lastQ)
+        #начинаем цикл, ходим по строкам и записываем данные в общий массив
+        for i in range (1, num_of_lines):
+            #массив для текущего альбома в цикле
+            current_album = []
 
-        #когда получили все параметры, находим переход на новую строку и делаем его новой стартовой позицией
-        idx_nl = file_string.find('\n',idx_lastQ)
-        index_start = idx_nl
-        album_array.append(current_album)
+            #в каждой строке 12 параметров, вытаскиваем их все
+            for a in range (0, 12):
+                #определяем начало и конец кажого параметра при помощи кавычек
+                #кавычка в начале
+                idx_firstQ = file_string.find('\"', index_start) + 1
+                #кавычка в конце
+                idx_lastQ = file_string.find('\"', idx_firstQ)
+                #получаем подстроку при помощи индексов кавычек
+                txt = file_string[idx_firstQ:idx_lastQ]
+                if txt != "" and a != 10 and a != 8 or a == 6 or a == 1 or a == 2 or a == 3 or a == 4:
+                    current_album.append(txt)
+                
+                index_start = file_string.find(",",idx_lastQ)
 
-    #создаем новую переменную для форматирования и вывода, копируем в неё данные из старой
-    album_display = album_array
+            #когда получили все параметры, находим переход на новую строку и делаем его новой стартовой позицией
+            idx_nl = file_string.find('\n',idx_lastQ)
+            index_start = idx_nl
+            album_array.append(current_album)
 
-    #объединяем наименования исполниелей
-    for i in range(len(album_display)):
-        space_char = ""
-        if album_display[i][1] != "":
-            space_char = " "
-        album_display[i][1] += space_char + album_display[i][2]
-        del album_display[i][2]
-        del album_display[i][2]
-        del album_display[i][2]
+        #создаем новую переменную для форматирования и вывода, копируем в неё данные из старой
+        album_display = album_array
+
+        #объединяем наименования исполниелей
+        for i in range(len(album_display)):
+            space_char = ""
+            if album_display[i][1] != "":
+                space_char = " "
+            album_display[i][1] += space_char + album_display[i][2]
+            del album_display[i][2]
+            del album_display[i][2]
+            del album_display[i][2]
 
 
-    #сортировка списка по алфавиту и году выпуска альбома
-    album_display = sorted(album_display,key=lambda x: (x[1],x[3]))
+        #сортировка списка по алфавиту и году выпуска альбома
+        album_display = sorted(album_display,key=lambda x: (x[1],x[3]))
 
-    return album_display
+        return album_display
+    else:
+        print("File does not exist")
+        return 0
 
 #ф-ция, вывод альбомов в виде таблицы
 def show_album_spreadsheet(array):
@@ -75,13 +81,13 @@ def basic_stats(array):
     #подисчитываем статистику
     #всего альбомов
     print ("Basic statistics")
-    albums_total_count = len(album_list)
+    albums_total_count = len(array)
     print("Albums total: " + str(albums_total_count) + " albums")
 
     #считаем среднюю оценку
     avg_total = 0
     for i in range (albums_total_count):
-        avg_total += int(album_list[i][4])
+        avg_total += int(array[i][4])
     avg_total = avg_total / albums_total_count
     avg_total = math.ceil(avg_total*10)/10
     print("Avg. score: " + str(avg_total) + " / 10")
@@ -91,8 +97,8 @@ def basic_stats(array):
 
     for score in range (1, 11):
         count = 0
-        for i in range (len(album_list)):
-            if album_list[i][4] == str(score):
+        for i in range (len(array)):
+            if array[i][4] == str(score):
                 count+=1
         all_scores_list.append(count)
 
@@ -106,30 +112,48 @@ def basic_stats(array):
         scores_line += " - " + str(all_scores_list[i]) + "({})".format(i+1)
         print(scores_line)
 
+def change_filename(new_filename):
+    global global_filename
+    global_filename = new_filename
+    global global_album_list
+    global_album_list = load_file(new_filename)
+    if (global_album_list != 0):
+        print("File name was successfully changed")
+
 #ф-ция, справка по командам
 def help():
     print("bs - Basic statistics")
     print ("as - Albums spreadsheet")
+    print ("chf - Change the name of the data file")
 
 #главная ф-ция
 def main_func():
+    print("Rate Your Music Parser v 0.1")
     command = ""
     while command != "exit":
-        print("input: ")
+        print("cmd: ")
         command = input()
         if command == "bs":
-            basic_stats(album_list)
+            if(global_album_list != 0):
+                basic_stats(global_album_list)
         elif command == "as":
-            show_album_spreadsheet(album_list)
+            if(global_album_list != 0):
+                show_album_spreadsheet(global_album_list)
+        elif command == "chf":
+            print("New file name: ")
+            filename = input()
+            change_filename(filename)
+        elif command == "help":
+            help()
         elif command == "exit":
             break
         else:
             print("No such command")
 
-        
-album_list = load_file("albums.txt")
+global_filename = "albums"
+global_album_list = load_file(global_filename)
 
-basic_stats(album_list)
+#запускаем главную ф-цию
 main_func()
 
 
