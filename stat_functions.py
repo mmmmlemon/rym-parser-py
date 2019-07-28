3a356d60014b751e30819d37efc852f7e45013ef
EgoriousZhuzh <egor_005@mail.ru>
2019-07-28 17:34:36 +1000

    Убрал рисование графиков в браузере, доработал график в консоли

--- stat_functions.py
+++ stat_functions.py
@@ -2,12 +2,27 @@ import math
 import plotly.graph_objects as go
 
 #глобальные переменные
+graph_symb = "■"
 #цвета для оценок
 score_colors = ['#820707', '#ff0000', '#ff8800', '#ffcc00', '#f6ff00', '#d4ff00', '#80ff00', '#22e32c', '#13bd46', '#00ffc3']
 #всплывающие заголовки для оценок
-score_hovertext = ['1/10 - Неистовое убожество', '2/10 - Полный хлам',
+score_hovertext = ['/10 - Неистовое убожество', '2/10 - Полный хлам',
 '3/10 - Позор', '4/10 - Плохо', '5/10 - Посредственно', '6/10 - Нормально', '7/10 - Хорошо',
 '8/10 - Отлично', '9/10 - Замечательно', '10/10 - Любимчик']
+
+#всякие глобальные функции
+#нарисовать график по массиву с данными
+
+def draw_graph(array_data, array_names):
+    for i in range(len(array_data)):
+        num = array_data[i]
+        num_round = math.ceil(num / 2)
+        #print(num_round)
+        scores_line = graph_symb
+        for ii in range(int(num_round)):
+            scores_line += graph_symb
+        scores_line += " - " + str(array_data[i]) + "({})".format(array_names[i])
+        print(scores_line)
 
 #ф-ция, базовая статистика
 def basic_stats(array):
@@ -36,15 +51,11 @@ def basic_stats(array):
                 count+=1
         all_scores_list.append(count)
         all_scores_names.append(score)
+        
+    #рисуем графики
+    print("\nОбщее количество оценок")
+    draw_graph(all_scores_list, all_scores_names)
 
-    layout = go.Layout(
-    xaxis = go.layout.XAxis(
-        tickmode = 'linear',
-        tick0 = 1,
-        dtick = 1
-    ))
 
-    
-    fig = go.Figure(data=go.Bar(x=all_scores_names,y=all_scores_list, marker_color=score_colors, 
-    hovertext = score_hovertext),layout_title_text="Общая статистика ({} альбомов)".format(albums_total_count), layout=layout)
-    fig.show()
+
+
